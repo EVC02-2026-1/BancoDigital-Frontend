da299acdf5aac7b2e44a213a949f1a6911a36d63
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2026-05-05 19:55:33 -0500

    Uncomment baseURL for API configuration

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:8081/api',
-  baseURL: 'https://bancodigital-backend.onrender.com/api'  
+  baseURL: 'https://bancodigital-backend.onrender.com/api',  
 });
 
 // Interceptor to add JWT to requests
